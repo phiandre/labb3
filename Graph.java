e9af4bda4b216093a050ec47deadd5bf11a75817
Philip Andreasson <phiandr@student.chalmers.se>
2017-11-29 18:07:13 +0100

    2017-11-29

--- Graph.java
+++ Graph.java
@@ -1,30 +1,44 @@
 import java.util.*;
 
 public class Graph {
-	private ArrayList<LinkedList<Pair>> listRep;
-	private HashMap<String,APrioMap<String,Integer>> hashRep; //= new HashMap<String,APrioMap<String,Integer>>()
+	private Map<String, ArrayList<Pair<String,Integer>>> mapRep;
+	//private ArrayList< LinkedList< Pair<String,Integer> > > listRep;
+	//private HashMap<String,APrioMap<String,Integer>> hashRep; //= new HashMap<String,APrioMap<String,Integer>>()
+	
     public Graph() { 
-		listRep = new ArrayList<LinkedList<Pair>>();
-		hashRep=new HashMap<String,APrioMap<String,Integer>>();
+		//listRep = new ArrayList< LinkedList< Pair<String,Integer> > >();
+		//hashRep = new HashMap<String,APrioMap<String,Integer>>();
+		//prioMapRep = new APrioMap<String, LinkedList< Pair<String,Integer> > >;
+		mapRep = new HashMap<String, ArrayList<Pair<String,Integer>>>();
 	}
     
-    public void addVertice(String label){ 
+    public void addVertice(String label){
 		/*
 		LinkedList<Pair> noNeighbors = new LinkedList<Pair>();
 		noNeighbors.add(new Pair(label,0)); //ditsanse 0 from it slef
-		listRep.add(noNeighbors);
-		*/
-		hashRep.put(label,new APrioMap<String,Integer>());
+		listRep.add(noNeighbors);*/
 		
+		//hashRep.put(label,new APrioMap<String,Integer>());
+		
+		//add the new vertice and an arrayList for the neighbors.
+		mapRep.put(label, new ArrayList<Pair<String,Integer>>());
 	}
     
     public void addEdge(String node1, String node2, int dist){ 
-		APrioMap A=hashRep.get(node1);
-		APrioMap B= hashRep.get(node2);
+		/*APrioMap A = hashRep.get(node1);
+		APrioMap B = hashRep.get(node2);
 		A.put(node2,dist);
 		B.put(node1,dist);
 		hashRep.put(node1,A);
-		hashRep.put(node2,B);
+		hashRep.put(node2,B);*/
+		
+		/*The graph is undirected, so should add a neighbor to both
+		 * node1 and node2.
+		 */
+		ArrayList<Pair<String,Integer>> neighbors1 = mapRep.get(node1);
+		ArrayList<Pair<String,Integer>> neighbors2 = mapRep.get(node2);
+		neighbors1.add(new Pair(node2,dist));
+		neighbors2.add(new Pair(node1,dist));
 	}
     
     public static class Path {
@@ -41,9 +55,12 @@ public class Graph {
         }
     }
     
-    public void shortestPath(String start, String dest){ 
+    public Path shortestPath(String start, String dest){ 
 		boolean[] known = new boolean[hashRep.size()];
 		int[] dv = new int[hashRep.size()];
+		
+		APrioMap<String,Integer> q = new APrioMap<String,Integer>();
+		q.put(start,0);
 		
 		for(int i=0; i<hashRep.size(); i++){
 			known[i] = false;
